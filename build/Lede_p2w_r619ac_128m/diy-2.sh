f1d9acbdbbbba1c0b4850a3ca81d65b40f4772cd
jerunshi <60541084+jerunshi@users.noreply.github.com>
2021-04-18 10:16:06 +0800

    Update diy-2.sh

--- build/Lede_p2w_r619ac_128m/diy-2.sh
+++ build/Lede_p2w_r619ac_128m/diy-2.sh
@@ -16,6 +16,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# bypass依赖
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
