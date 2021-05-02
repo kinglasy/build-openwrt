565128fbe0761db9f720a8ff90b60f7f0921c8e1
kinglasy <37769432+kinglasy@users.noreply.github.com>
2021-05-02 09:34:39 +0800

    Update diy-1.sh

--- build/Lede_phicomm_n1/diy-1.sh
+++ build/Lede_phicomm_n1/diy-1.sh
@@ -7,3 +7,21 @@
 # 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
 # 软件包地址：https://github.com/281677160/openwrt-package
 # 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
+git clone https://github.com/ITdesk01/jd_openwrt_script.git package/jd_openwrt_script
+git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+git clone https://github.com/garypang13/smartdns-le package/smartdns-le
+git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
